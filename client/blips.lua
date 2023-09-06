04b55bed34d2a971bb8f91c960873596a455c194
Thijmen <53520119+NietThijmen@users.noreply.github.com>
2023-09-06 19:46:25 +0200

    fix! blips not showing

--- client/blips.lua
+++ client/blips.lua
@@ -1,6 +1,6 @@
 CreateThread(function()
     for k, v in pairs(Config.RentalLocations) do
-        if not v.blip.enabled then goto continue end
+        if not v.blip.show then goto continue end
         local blip = AddBlipForCoord(v.blip.coords.x, v.blip.coords.y, v.blip.coords.z)
         SetBlipSprite(blip, v.blip.sprite)
         SetBlipDisplay(blip, 4)
